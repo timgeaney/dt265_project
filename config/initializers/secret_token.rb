8c8abe6f76eb537b303b0388b738eaecc0726037
Tim Geaney <timgeaney24@gmail.com>
2013-01-16 15:41:58 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dt265Project::Application.config.secret_token = 'eee3bf38b4f4e76de06caf9a9a6a61374b4fffb73085bed8354c97419fb7b1cafd06b70a0c34915390ed670e3ab05623dbc7b83ba6bf3fa4e4f5d68f1034b59b'
